e980e2e889b5fb242968b5442a181e97db202fee
Kanak Agrawal <akanak101@gmail.com>
2022-01-14 19:45:39 +0530

    Updated Atlas Database

--- server/index.js
+++ server/index.js
@@ -15,8 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://akanak101:akanak101@cluster0.yzcqw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
